f312d74b3d863580a3528d68353d4411cf036189
Jonathan Arnett <jonarnett90@gmail.com>
2015-11-12 10:57:08 -0500

    Made all migrations reversible

--- db/migrate/20151105160535_change_phoenix_floating_ip_to_string.rb
+++ db/migrate/20151105160535_change_phoenix_floating_ip_to_string.rb
@@ -1,6 +1,11 @@
 class ChangePhoenixFloatingIpToString < ActiveRecord::Migration
-  def change
+  def up
     rename_column :phoenixes, :floating_ip_id, :floating_ip
     change_column :phoenixes, :floating_ip, :string
+  end
+
+  def down
+    remove_column :phoenixes, :floating_ip
+    add_column :phoenixes, :floating_ip_id, :integer
   end
 end
--- db/migrate/20151106004454_add_devise_to_users.rb
+++ db/migrate/20151106004454_add_devise_to_users.rb
@@ -44,6 +44,24 @@ class AddDeviseToUsers < ActiveRecord::Migration
   def self.down
     # By default, we don't want to make any assumption about how to roll back a migration when your
     # model already existed. Please edit below which fields you would like to remove in this migration.
-    raise ActiveRecord::IrreversibleMigration
+    change_table(:users) do |t|
+      ## Database authenticatable
+      t.remove :email
+      t.remove :encrypted_password
+
+      ## Recoverable
+      t.remove :reset_password_token
+      t.remove :reset_password_sent_at
+
+      ## Rememberable
+      t.remove :remember_created_at
+
+      ## Trackable
+      t.remove :sign_in_count
+      t.remove :current_sign_in_at
+      t.remove :last_sign_in_at
+      t.remove :current_sign_in_ip
+      t.remove :last_sign_in_ip
+    end
   end
 end
