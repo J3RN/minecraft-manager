0005bc57bc7d00c42a9a7bb307f044737f6340f7
Jonathan Arnett <jonarnett90@gmail.com>
2015-11-12 11:04:23 -0500

    Added more users in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,22 @@
-User.create!({
+if User.all.any?
+  User.destroy_all
+end
+
+User.create!([
+  {
     username: "J3RN",
     email: "jonarnett90@gmail.com",
     access_token: ENV["DO_TOKEN"],
     password: "blargblarg"
-})
+  },
+  {
+    username: "TestUser",
+    email: "test@example.com",
+    password: "testtest"
+  },
+  {
+    username: "TestTest",
+    email: "test@example.org",
+    password: "testtest"
+  },
+])
