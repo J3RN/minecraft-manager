2127175149575814bd903fd54714d7a7a906459f
Jonathan Arnett <jonarnett90@gmail.com>
2015-11-12 00:06:49 -0500

    Added Sinatra dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   devise_for :users
   root to: "main#index"
 
   resources :phoenixes, except: [:show]
+
+  authenticate :user do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 end
