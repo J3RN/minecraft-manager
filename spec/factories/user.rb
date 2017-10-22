091c05cae9d4ab52c6d863e88a0fd05a331d541d
Jonathan Arnett <jonarnett90@gmail.com>
2017-10-22 00:22:06 -0400

    Add factory for user with phoenixes

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -6,6 +6,16 @@ FactoryGirl.define do
 
     factory :user_with_access_token, aliases: [:owner] do
       access_token 'asdfasfdasdfasdfasdf'
+
+      factory :user_with_phoenixes do
+        transient do
+          phoenixes_count 1
+        end
+
+        after(:create) do |user, evaluator|
+          create_list(:phoenix, evaluator.phoenixes_count, owner: user)
+        end
+      end
     end
   end
 end
