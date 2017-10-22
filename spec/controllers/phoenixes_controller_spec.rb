6e19e6adfd39d115a38d39ddf67db248189f31d0
Jonathan Arnett <jonarnett90@gmail.com>
2017-10-22 01:49:33 -0400

    Flesh out tests

--- spec/controllers/phoenixes_controller_spec.rb
+++ spec/controllers/phoenixes_controller_spec.rb
@@ -2,6 +2,12 @@ describe PhoenixesController do
   before do
     @user = create(:user_with_phoenixes)
     @phoenix = @user.owned_phoenixes.first
+
+    @user_without_access_token = create(:user)
+
+    other_user = create(:user_with_phoenixes)
+    @unowned_phoenix = other_user.owned_phoenixes.first
+
     sign_in @user
   end
 
@@ -20,30 +26,82 @@ describe PhoenixesController do
   end
 
   describe 'GET new' do
-    before do
-      VCR.use_cassette('droplet fetch') do
-        get :new
+    context 'with access_token' do
+      before do
+        VCR.use_cassette('droplet fetch') do
+          get :new
+        end
+      end
+
+      it 'renders sucessfully' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'builds a new phoenix' do
+        expect(assigns(:phoenix)).to be_a_new(Phoenix)
       end
     end
 
-    it 'renders sucessfully' do
-      expect(response).to have_http_status(:success)
+    context 'without access_token' do
+      before do
+        sign_out @user
+        sign_in @user_without_access_token
+        VCR.use_cassette('droplet fetch') do
+          get :new
+        end
+      end
+
+      it 'redirects back to the index' do
+        expect(response).to redirect_to(phoenixes_url)
+      end
+    end
+  end
+
+  describe 'POST create' do
+    before do
+      post :create, phoenix: attributes_for(:phoenix)
     end
 
-    it 'builds a new phoenix' do
-      expect(assigns(:phoenix)).to be_a_new(Phoenix)
+    it 'redirects to index' do
+      expect(response).to redirect_to(phoenixes_url)
+    end
+
+    it 'sets the owner on the new phoenix' do
+      expect(assigns(:phoenix).owner).to eq(@user)
+    end
+
+    it 'adds the owner to the list of users' do
+      expect(assigns(:phoenix).users).to include(@user)
     end
   end
 
   describe 'GET edit' do
-    before do
-      VCR.use_cassette('droplet fetch') do
-        get :edit, { id: @phoenix.id }
+    context 'requesting owned phoenix' do
+      before do
+        VCR.use_cassette('droplet fetch') do
+          get :edit, { id: @phoenix.id }
+        end
+      end
+
+      it 'renders sucessfully' do
+        expect(response).to have_http_status(:success)
+      end
+
+      it 'loads the requested phoenix' do
+        expect(assigns(:phoenix)).to eq(@phoenix)
       end
     end
 
-    it 'renders sucessfully' do
-      expect(response).to have_http_status(:success)
+    context 'requesting unowned phoenix' do
+      before do
+        VCR.use_cassette('droplet fetch') do
+          get :edit, { id: @unowned_phoenix.id }
+        end
+      end
+
+      it 'redirects back to the index' do
+        expect(response).to redirect_to(phoenixes_url)
+      end
     end
   end
 end
